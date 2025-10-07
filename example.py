dad36dc5999946888c9d3f68f02a2b9adfda8f0e
Eduardo Gomez <107379264+EduardoHGomez@users.noreply.github.com>
2025-10-07 08:39:05 -0600

    Update example.py

--- example.py
+++ example.py
@@ -1,19 +1,39 @@
-import random
-def startGame():
-  number = random.randint(1, 20)
-  guess = 0
-  attempts = 0
-  print("Adivina el número entre 1 y 20")
-  while guess != number:
-    guess = int(input("Ingresa tu intento: "))
-    attempts += 1
+def getInputNumber():
+    while True:
+        s = input("Ingresa el numero: ")
+        # Validat input que sea correcto
+        try:
+            return int(s)
+        except ValueError:
+            print("Ingresa un numero, intenta de nuevo")  
+
+def validateGuess(guess: int):
+    result = False
     if guess < number:
-      print("Muy bajo")
-      elif guess > number:
+        print("Muy bajo")
+    elif guess > number:
         print("Muy alto")
-      elif guess == number:
+    elif guess == number:
         print("¡Correcto!")
-    else:
-      print("Error")
-      print("Número de intentos:", attempts)
-startGame()
+        result = True
+    return result
+
+def startGame():
+    number = random.randint(1, 20)
+    guess = 0
+    attempts = 0
+    print("Adivina el número entre 1 y 20")
+    while guess != number:
+        userNumber = getInputNumber()
+        attempts += 1
+        result = validateGuess(userNumber)
+        print("Número de intentos:", attempts)
+        if result:
+            return
+        
+
+# DMain the python
+if __name__ == "__main__":
+    print("Starting game...")
+    startGame()
+  
